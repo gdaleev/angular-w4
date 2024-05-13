ded6f34404a67a59fb78a4b9a8a96ed4c42b6461
Georgi Daleev <gdaleev2346@gmail.com>
2024-05-13 08:58:05 +0300

    Added logic for lighting intervals

--- src/app/bottom-traffic-light/bottom-traffic-light.component.ts
+++ src/app/bottom-traffic-light/bottom-traffic-light.component.ts
@@ -1,12 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bottom-traffic-light',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './bottom-traffic-light.component.html',
   styleUrl: './bottom-traffic-light.component.css'
 })
 export class BottomTrafficLightComponent {
+  redLightColor: string = 'black';
+  yellowLightColor: string = 'black';
+  greenLightColor: string = 'black';
+  colorInterval: any;
 
+  constructor() {
+    this.startColorInterval();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.colorInterval);
+  }
+
+  private startColorInterval() {
+    let currentPhase = 1;
+
+    this.colorInterval = setInterval(() => {
+      if (currentPhase == 1) {
+        this.greenLightColor = 'green';
+        setTimeout(() => {
+          currentPhase = 2;
+          this.yellowLightColor = 'yellow';
+          this.greenLightColor = "black"
+        }, 5000);
+      } else if (currentPhase == 2) {
+        setTimeout(() => {
+          currentPhase = 3;
+          this.redLightColor = "red"
+          this.yellowLightColor = "black"
+          this.greenLightColor = 'black';
+        }, 2000);
+      } else if (currentPhase == 3) {
+        setTimeout(() => {
+          currentPhase = 4;
+          this.yellowLightColor = "yellow"
+        }, 5000);
+      } else {
+        setTimeout(() => {
+          currentPhase = 1;
+          this.redLightColor = "black"
+          this.yellowLightColor = "black"
+          this.greenLightColor = "green"
+        }, 2000)
+      }
+    });
+  }
 }
--- src/app/left-traffic-light/left-traffic-light.component.ts
+++ src/app/left-traffic-light/left-traffic-light.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-left-traffic-light',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './left-traffic-light.component.html',
-  styleUrl: './left-traffic-light.component.css'
+  styleUrl: './left-traffic-light.component.css',
 })
-export class LeftTrafficLightComponent {
+export class LeftTrafficLightComponent implements OnDestroy {
+  redLightColor: string = 'black';
+  yellowLightColor: string = 'black';
+  greenLightColor: string = 'black';
+  colorInterval: any;
 
+  constructor() {
+    this.startColorInterval();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.colorInterval);
+  }
+
+  private startColorInterval() {
+    let currentPhase = 1;
+
+    this.colorInterval = setInterval(() => {
+      if (currentPhase == 1) {
+        this.redLightColor = 'red';
+        setTimeout(() => {
+          currentPhase = 2;
+          this.yellowLightColor = 'yellow';
+        }, 5000);
+      } else if (currentPhase == 2) {
+        setTimeout(() => {
+          currentPhase = 3;
+          this.redLightColor = "black"
+          this.yellowLightColor = "black"
+          this.greenLightColor = 'green';
+        }, 2000);
+      } else if (currentPhase == 3) {
+        setTimeout(() => {
+          currentPhase = 4;
+          this.yellowLightColor = "yellow"
+          this.greenLightColor = 'black';
+        }, 5000);
+      } else {
+        setTimeout(() => {
+          currentPhase = 1;
+          this.redLightColor = "red"
+          this.yellowLightColor = "black"
+        }, 2000)
+      }
+    });
+  }
 }
